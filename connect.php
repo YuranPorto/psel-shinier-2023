bd65cad957404a115ddf56e8804ec9c702668c97
yuranporto <yuranporto@gmail.com>
2023-03-27 20:32:38 -0300

    Verificando busca no banco de dados

--- connect.php
+++ connect.php
@@ -1,12 +1,19 @@
 <?php 
 
-$dsn = 'firebird:dbname=127.0.0.1/3050:C:\Program Files\Firebird\Firebird_4_0\data\new_db_name_test.fdb';
+$dsn = 'firebird:dbname=127.0.0.1/3050:C:\Program Files\Firebird\Firebird_4_0\data\DB_TESTE_SHINIER.fdb';
 $username = 'SYSDBA';
 $password = 'masterkey';
 
 try {
     $dbh = new PDO($dsn, $username, $password);
-    echo "Conexão estabelecida!";
+    $sql = 'SELECT NOME FROM EMD101 ORDER BY NOME DESC ROWS 5';
+    $stmt = $dbh->query($sql);
+    $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($resultados as $resultado){
+        echo $resultado['NOME'];
+    }
+
 } catch (PDOException $e) {
     echo "Erro de conexão: " . $e->getMessage();
 }
